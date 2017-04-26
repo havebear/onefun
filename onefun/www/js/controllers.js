c45ef480489a8c7cae86b69abf371bb54941e4ad
havebear <616749285@qq.com>
2017-04-26 15:32:26 +0800

    新增添加教程页面 -- bgg

--- onefun/www/js/controllers.js
+++ onefun/www/js/controllers.js
@@ -32,12 +32,20 @@ angular.module('starter.controllers', [])
 
 .controller('TutorialDetailCtrl', function($scope,$state,$stateParams,$ionicViewSwitcher) {
 	//接受数据，是否收藏
-	$scope.isActive = true;
-	$scope.shoucang = function(){
-		if($scope.isActive){
-			$scope.isActive = false;
+	$scope.isCollection = true;
+	$scope.isFollow = true;
+	$scope.collection = function(){
+		if($scope.isCollection){
+			$scope.isCollection = false;
 		}else{
-			$scope.isActive = true;
+			$scope.isCollection = true;
+		}
+	}
+	$scope.follow = function(){
+		if($scope.isFollow){
+			$scope.isFollow = false;
+		}else{
+			$scope.isFollow = true;
 		}
 	}
 	//同级解决次级页面出现底部tab方案
@@ -50,8 +58,9 @@ angular.module('starter.controllers', [])
 
 .controller('TutorialListCtrl', function($scope,$stateParams) {
 	var type = $stateParams.type || 0;
-	var types = {"hot": "本周热门", "friend": "关注动态", "0000001": "布艺", "0000002": "皮艺", "0000003": "纸艺", "0000004": "木艺", "0000005": "饰品", "0000006": "编织", "0000007": "模型", "0000008": "旧物"};
+	var types = {"hot": "本周热门", "friend": "关注动态", 1: "布艺", 2: "皮艺", 3: "纸艺", 4: "木艺", 5: "饰品", 6: "编织", 7: "模型", 8: "旧物"};
 	$scope.pagetitle = types[type];
+	
 })
 
 .controller('MyFollowCtrl', function($scope, $ionicPopup, $timeout) {
@@ -75,7 +84,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('MyMessageCtrl', function($scope, $ionicPopup, $timeout) {
-	$scope.deleteLiuyan = function(){
+	$scope.deleteMessage = function(){
 		var confirmPopup = $ionicPopup.confirm({
 	     title: '一坊',
 	     template: '确认删除',
@@ -99,7 +108,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('MyTutorialDetailCtrl', function($scope, $ionicPopup, $timeout) {
-	$scope.quxiao = function(){
+	$scope.collection = function(){
 		var confirmPopup = $ionicPopup.confirm({
 	     title: '一坊',
 	     template: '取消收藏',
@@ -120,11 +129,12 @@ angular.module('starter.controllers', [])
 
 .controller('LoginCtrl', function($scope,$state,Md5) {
 	$scope.customer = {
-		name: "",
-		pwd: ""
+		name: '',
+		pwd: ''
 	}
 	$scope.login = function(){
 		console.log(Md5.hex_md5($scope.customer.pwd));
+		console.log(00001);
 	}
 })
 
@@ -133,7 +143,42 @@ angular.module('starter.controllers', [])
 })
 
 .controller('AddTutorialCtrl', function($scope) {
-	
+	$scope.course = {
+		course_name: '',
+		course_name: '',
+		type: '',
+		course_material: '',
+		steps:[
+			{
+//				order: 1,
+				img: '',
+				describe: '我是第一'
+			},
+			{
+//				order: 2,
+				img: '',
+				describe: '我是第二'
+			},
+			{
+//				order: 3,
+				img: '',
+				describe: '我是第三'
+			}
+		]
+	};
+	//添加一个步骤
+	$scope.addStep = function(){
+		var nextstep = {
+			order: $scope.course.steps.length + 1,
+			img: '',
+			describe: ''
+		}
+		$scope.course.steps.push(nextstep);
+	}
+	//删除当前步骤
+	$scope.deleteStep = function(step){
+		$scope.course.steps.splice($scope.course.steps.indexOf(step),1);
+	}
 })
 
 .controller('AddMessageCtrl', function($scope, $ionicPopup, $timeout) {
